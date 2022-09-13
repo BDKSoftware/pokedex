c1ec35c4f87018d4281700419dea6642f45aeede
Bradley Donn Kukuk <brad@bdksoftware.com>
2022-09-13 15:41:20 -0600

    Chandes to PokemonScreen

--- pages/pokemon/[pokemon].jsx
+++ pages/pokemon/[pokemon].jsx
@@ -1,27 +1,56 @@
+import Link from "next/link";
+import Image from "next/image";
 import React, { useState } from "react";
 import classes from "../../styles/PokemonScreen.module.css";
 import { setPokemonColorbyType } from "../../utils/setPokemonColorbyType";
 
 const PokemonScreen = ({ pokemon }) => {
   let mainType = pokemon.types[0].type.name;
-  let pokemonTypeColor = setPokemonColorbyType(mainType);
+  let pokemonTypeColor = setPokemonColorbyType(mainType).toString() + "B3";
   let types = pokemon.types
     .map(
       (type) => type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1)
     )
     .join(", ");
-  let pokemonName = pokemon.name;
+  let pokemonName =
+    pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+  let height = pokemon.height;
+  let weight = pokemon.weight;
+  let id = pokemon.id;
+  let imgURL = pokemon.sprites.front_default;
+  let stats = pokemon.stats;
 
-  console.log(pokemon.types[0].type.name);
   return (
     <div className={classes.pokemonScreenContainer}>
       <div
         className={classes.pokemonContainer}
         style={{
-          backgroundColor: pokemonTypeColor + "B3",
+          backgroundColor: pokemonTypeColor,
           borderColor: "white",
         }}
-      ></div>
+      >
+        <div className={classes.top}>
+          <div className={classes.topLeft}>
+            <div className={classes.pokemonDetailCard}>
+              <img src={imgURL} alt={pokemonName} className={classes.sprite} />
+            </div>
+          </div>
+          <div className={classes.topRight}>
+            <div className={classes.pokemonDetailCard}>
+              <h1 className={classes.titleText}>
+                {pokemonName} #{id}
+              </h1>
+              <h2 className={classes.titleText}>Height: {height}</h2>
+              <h2 className={classes.titleText}>Weight: {weight}</h2>
+              <h2 className={classes.titleText}>Types: {types}</h2>
+            </div>
+          </div>
+        </div>
+        <div className={classes.bottom}></div>
+      </div>
+      <Link href="/">
+        <span className={classes.searchText}>Back To Search</span>
+      </Link>
     </div>
   );
 };
